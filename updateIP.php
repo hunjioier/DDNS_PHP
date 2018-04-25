81b0a1705c54db208fc8c384b541f1972d56e8ca
hunji <hunjioier@gmail.com>
2018-04-25 21:25:56 +0800

    update code , solve update dns

--- updateIP.php
+++ updateIP.php
@@ -3,11 +3,14 @@
     $conf_arr=json_decode($config,true);
     $LOG=$conf_arr["log"];
     $IP_TEMP=$conf_arr["ip_temp"];
+    $SSH_KEY=$conf_arr["ssh_key"];
+    $SSH_PWD=$conf_arr["ssh_pwd"];
+    $DOMAIN=$conf_arr["domain"];
+    $NAME=$conf_arr["name"];
+    $TTL=$conf_arr["ttl"];
 
     $tempIpAdress=file_get_contents("http://ip-api.com/json");
     $arr=json_decode($tempIpAdress,true);
-    //var_dump($arr);
-    //die;
 
     $ip=$arr["query"];
     if($ip=="")
@@ -34,7 +37,7 @@
             fclose($hander);
             $content=date("Y-m-d h:i:s")." ip from ".$tempip." change to ".$ip." prepare to change A record\n";
             file_put_contents($LOG."updateIP".date("Ymd").".log", $content,FILE_APPEND);
-            updateARecord();
+            updateARecord($ip,$SSH_KEY,$SSH_PWD,$DOMAIN,$NAME,$TTL,$LOG);
         }
     }
     else
@@ -44,10 +47,22 @@
         fclose($hander);
         $content=date("Y-m-d h:i:s")." set up ip_config ip is ".$ip." prepare to change A record \n";
         file_put_contents($LOG."updateIP".date("Ymd").".log", $content,FILE_APPEND);
-        updateARecord();
+        updateARecord($ip,$SSH_KEY,$SSH_PWD,$DOMAIN,$NAME,$TTL,$LOG);
     }
-    function updateARecord()
+    function updateARecord($ip,$SSH_KEY,$SSH_PWD,$DOMAIN,$NAME,$TTL,$LOG)
     {
-        //todo
+        $cmd="curl -i -s -X PUT -H \"Authorization: sso-key ".$SSH_KEY.":".$SSH_PWD."\" -H \"Content-Type: application/json\" -d '[{\"data\":\"'".$ip."'\",\"ttl\":".$TTL."}]' \"https://api.godaddy.com/v1/domains/".$DOMAIN."/records/A/".$NAME."\"";
+        $info="";
+        exec($cmd,$info);
+        if($info[0]=="HTTP/1.1 200 OK")
+        {
+            $content=date("Y-m-d h:i:s")." ip update success ".$ip."\n";
+            file_put_contents($LOG."updateIP".date("Ymd").".log", $content,FILE_APPEND);
+        }
+        else
+        {
+            $content=date("Y-m-d h:i:s")." ip update error\n ".$ip.implode(" ",$info)."\n";
+            file_put_contents($LOG."updateIP".date("Ymd").".log.wf", $content,FILE_APPEND);
+        }
     }
 ?>
