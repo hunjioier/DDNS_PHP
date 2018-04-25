43221c7b11a747e879a5ba2fbaf4d330e1292491
hunji <hunjioier@gmail.com>
2018-04-25 13:44:47 +0800

    add log and use ip-api to get ip

--- updateIP.php
+++ updateIP.php
@@ -1,38 +1,50 @@
 <?php
-    $value=@$_POST["pass"];
-    if($value=="")
-        exit;
-	if($value==="testDDNS")
-	{
-		$ip=$_SERVER["REMOTE_ADDR"];
-		if(file_exists("/var/www/html/ip_conf")==true)
-		{
-			$hander=fopen("/var/www/html/ip_conf","r");
-			$tempip=fgets($hander);
-			if($tempip===$ip)
-			{
-				fclose($hander);
+    const LOG="/home/a/log/updateIP";
+    const IPCONF="/home/a/ip_conf";
 
-				echo "toule";
-			}
-			else
-			{
-				fclose($hander);
-				$hander=fopen("/var/www/html/ip_conf","w");
-				fwrite($hander,$ip);
-				fclose($hander);
+    $tempIpAdress=file_get_contents("http://ip-api.com/json");
+    $arr=json_decode($tempIpAdress,true);
+    //var_dump($arr);
+    //die;
 
-				echo "gengxin";
-				//todo
-			}
-		}
-		else
-		{
-			$hander=fopen("/var/www/html/ip_conf","w");
-			fwrite($hander,$ip);
-			fclose($hander);
-
-			echo "xinjian";
-		}
-	}
+    $ip=$arr["query"];
+    if($ip=="")
+    {
+        $content=date("Y-m-d h:i:s")." get ip faild\n";
+        file_put_contents(LOG.date("Ymd").".log.wf", $content,FILE_APPEND);
+        exit(1);
+    }
+    if(file_exists(IPCONF)==true)
+    {
+        $hander=fopen(IPCONF,"r");
+        $tempip=fgets($hander);
+        if($tempip===$ip)
+        {
+            fclose($hander);
+            $content=date("Y-m-d h:i:s")." ip not change ".$ip."\n";
+            file_put_contents(LOG.date("Ymd").".log", $content,FILE_APPEND);
+        }
+        else
+        {
+            fclose($hander);
+            $hander=fopen(IPCONF,"w");
+            fwrite($hander,$ip);
+            fclose($hander);
+            $content=date("Y-m-d h:i:s")." ip from ".$tempip." change to ".$ip." prepare to change A record\n";
+            file_put_contents(LOG.date("Ymd").".log", $content,FILE_APPEND);
+            updateARecord();
+        }
+    }
+    else
+    {
+        $hander=fopen(IPCONF,"w");
+        fwrite($hander,$ip);
+        fclose($hander);
+        $content=date("Y-m-d h:i:s")." set up ip_config ip is ".$ip." prepare to change A record \n";
+        file_put_contents(LOG.date("Ymd").".log", $content,FILE_APPEND);
+        updateARecord();
+    }
+    function updateARecord()
+    {
+    }
 ?>
